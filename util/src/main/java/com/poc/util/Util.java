34ce6c8b2464b41cd9fb9ec9b1c7d671b77438d1
Adrien Vergnaud <a.vergnaud@gmail.com>
2018-08-25 11:43:36 +0200

    complet

---
+++ util/src/main/java/com/poc/util/Util.java
@@ -0,0 +1,12 @@
+package com.poc.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Util {
+    final Logger logger = LoggerFactory.getLogger(Util.class);
+
+    public void uneMethode() {
+        logger.info("hello from util");
+    }
+}
